ece3f46234ea47d7600563cd2963615620b753ec
davybrion <ralinx@davybrion.com>
2010-08-26 23:17:22 +0200

    the actual ruby code... tests will be committed later since i still have to write them (shame on me) and i'm still figuring out RSpec

---
+++ lib/eventpublisher.rb
@@ -0,0 +1,87 @@
+module EventPublisher
+	class Event
+		attr_reader :name
+
+		def initialize(name)
+			@name = name
+			@handlers = []
+		end
+
+		def add(method=nil, &block)
+			@handlers << method if method
+			@handlers << block if block
+		end
+
+		def remove(method)
+			@handlers.delete method if method
+		end
+
+		def trigger(*args)
+			@handlers.each { |handler| handler.call *args }
+		end	
+	end	
+
+	def subscribe_all(subscriber)
+		each_suitable_handler(subscriber) do |event_symbol, method_symbol|
+			subscribe event_symbol, subscriber.method(method_symbol)
+		end
+	end
+	
+	def unsubscribe_all(subscriber)
+		each_suitable_handler(subscriber) do |event_symbol, method_symbol|
+			unsubscribe event_symbol, subscriber.method(method_symbol)
+		end
+	end
+	
+	def subscribe(symbol, method=nil, &block)
+		event = send(symbol)
+		event.add method if method
+		event.add block if block
+	end
+
+	def unsubscribe(symbol, method)
+		event = send(symbol)
+		event.remove method
+	end
+
+	private
+	
+	def each_suitable_handler(subscriber)
+		possible_handlers = subscriber.class.instance_methods.select { |name| name =~ /\w_handler/ }
+		possible_handlers.each do |method_name|
+			event_name = /(?<event_name>.*)_handler/.match(method_name)[:event_name]	
+			if EVENTS.include? event_name.to_sym
+				yield event_name.to_sym, method_name.to_sym
+			end
+		end
+	end
+	
+	def trigger(symbol, *args)
+		event = send(symbol)
+		event.trigger *args
+	end
+	
+	self.class.class_eval do
+		EVENTS = []
+
+		def event(symbol)
+			getter = symbol
+			variable = :"@#{symbol}"
+			EVENTS << symbol
+
+			define_method getter do
+				event = instance_variable_get variable
+
+				if event == nil
+					event = Event.new(symbol.to_s)
+					instance_variable_set variable, event
+				end
+
+				event
+			end
+			
+			private getter
+		end
+	end
+end
+
